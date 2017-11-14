1aeb34d5dd33f71d45890c515f235e8d98a76a63
shannonphu <shannonphu@sbcglobal.net>
2017-11-13 20:59:23 -0800

    added public/ for css + js files and linked in view/server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require("express");
 var app = express();
 
 app.set('view engine', 'hbs');
+app.use(express.static('public'));
 
 app.listen(3000, (request, response) => {
 	console.log("Server is listening on port 3000. Go to http://localhost:3000/");
